ff1026f8558e1726f6fd3f13dba5df315fdc9707
mayordeleke <mayordeleke@gmail.com>
2025-10-17 13:56:08 +0100

    initial commit: PHP hello world with variables

---
+++ hello world.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>hello world</title>
+    </head>
+<body>
+    <h1>  Mayordeleke</h1>
+    <?php
+     //my first hello world in php
+        echo "Hello World";
+         echo "<h1> APENA AZEEZ </h1>", " HOW ARE YOU MY FRIEND <br>";
+         // $y = (34+3); // this code is adding two numebr
+          //echo ($y) # we are displaying a varibale value....
+       ECHO "Hello World!<br>";
+       echo "Hello World!<br>";
+      EcHo "Hello World!<br>";
+
+    ?>
+    <?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+$x = 5 /* + 15 */ + 5;
+echo $x ; "<br>";
+$name = "azeez";
+echo "my name is " . $name . "<br>";
+?>
+</body>
+</html>
